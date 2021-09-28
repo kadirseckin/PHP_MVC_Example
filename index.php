ca5d22a12897bac1567b2d7750c4453f593931bd
Kadir Doğuş Seçkin <kadir.seckin44@gmail.com>
2021-09-28 20:51:24 +0300

    add autoload file

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-     function loadLibraries($className){
-          $fileName='system/libs/'.$className.'.php';
-          if(file_exists($fileName)){
-               include_once $fileName;
-          }     
-     }
-     function loadModals($className){
-          $fileName='app/model/'.$className.'.php';
-          if(file_exists($fileName)){
-               include_once $fileName;
-          }     
-     }
-     
-     spl_autoload_register('loadLibraries');
-     spl_autoload_register('loadModals');
+    include_once 'system/libs/AutoLoad.php';
 
      $router=new Router([
           ["home","Home","get"],
---
+++ system/libs/AutoLoad.php
@@ -0,0 +1,19 @@
+<?php 
+
+ 	function loadLibraries($className){
+          $fileName='system/libs/'.$className.'.php';
+          if(file_exists($fileName)){
+               include_once $fileName;
+          }     
+     }
+     
+     function loadModals($className){
+          $fileName='app/model/'.$className.'.php';
+          if(file_exists($fileName)){
+               include_once $fileName;
+          }     
+     }
+     
+     spl_autoload_register('loadLibraries');
+     spl_autoload_register('loadModals');
+ ?>
